ba9dccd0e3b8fc481d394a9eb649003ba9729db2
Viet Tran <tdviet12@gmail.com>
2017-12-18 23:28:00 +0800

    init some class

--- src/main/java/com/github/viettd/exchange_rates/controller/ExchangeRatesServlet.java
+++ src/main/java/com/github/viettd/exchange_rates/controller/ExchangeRatesServlet.java
@@ -4,6 +4,7 @@ import com.github.viettd.exchange_rates.bean.Response;
 import com.github.viettd.exchange_rates.common.Constant;
 import com.github.viettd.exchange_rates.utils.Utils;
 import com.google.gson.GsonBuilder;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 
 import javax.servlet.http.HttpServlet;
@@ -12,9 +13,14 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import static org.apache.commons.codec.CharEncoding.UTF_8;
+
 public class ExchangeRatesServlet extends HttpServlet {
 
     private static final Logger LOGGER = Logger.getLogger(ExchangeRatesServlet.class);
+    public static final String CURRENT_RATES_END_POINT = "current_rates";
+    public static final String JSON_CONTENT_TYPE_HEADER = "application/json; charset=utf-8";
+    public static final String CURRENCY_PARAM = "currency";
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
@@ -38,15 +44,22 @@ public class ExchangeRatesServlet extends HttpServlet {
 
     private void handle(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         long startTime = System.currentTimeMillis();
-        resp.setCharacterEncoding("utf-8");
-        resp.setContentType("application/json; charset=utf-8");
+        resp.setCharacterEncoding(UTF_8);
+        resp.setContentType(JSON_CONTENT_TYPE_HEADER);
         Response response = new Response();
         StringBuilder log = new StringBuilder();
 
 
         Utils.appendLog(log, String.format("%s %s %s", Utils.getClientIp(req), req.getMethod(), Utils.getUrl(req)));
         try {
-
+            if (req.getPathInfo().equalsIgnoreCase(CURRENT_RATES_END_POINT)) {
+                String currency = req.getParameter(CURRENCY_PARAM);
+                if (StringUtils.isBlank(currency)) {
+                    // TODO: get list exchange rates
+                } else {
+                    // TODO: get exchange rate for desired currency
+                }
+            }
         } catch (Exception e) {
             response = new Response(Constant.CODE_ERROR, e.getMessage());
             Utils.appendExceptionLog(log, "handle", e);
--- src/main/java/com/github/viettd/exchange_rates/utils/Utils.java
+++ src/main/java/com/github/viettd/exchange_rates/utils/Utils.java
@@ -1,12 +1,47 @@
 package com.github.viettd.exchange_rates.utils;
 
 
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.log4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.InputStream;
+
+import static org.apache.commons.codec.Charsets.UTF_8;
 
 public class Utils {
+
+    private static final Logger LOGGER = Logger.getLogger(Utils.class);
+
+    public static String callGetApi(String url) {
+        String response = "";
+        try {
+            RequestConfig conf = RequestConfig.custom()
+                    .setSocketTimeout(90000)
+                    .setConnectTimeout(90000)
+                    .setConnectionRequestTimeout(90000)
+                    .build();
+            CloseableHttpClient client = HttpClientBuilder.create().setDefaultRequestConfig(conf).build();
+            HttpUriRequest req = new HttpGet(url);
+            try (CloseableHttpResponse res = client.execute(req)) {
+                HttpEntity entity = res.getEntity();
+                InputStream inputStream = entity.getContent();
+                response = IOUtils.toString(inputStream, UTF_8);
+            }
+        } catch (Exception e) {
+            LOGGER.error("callGetApi error:", e);
+        }
+        return response;
+    }
 
     public static void appendLog(StringBuilder logBuilder, String strLog) {
         logBuilder.append(strLog).append("\t");
